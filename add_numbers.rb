29f79bf1c64469bb754d83d0b1fb0a1b2bbe9934
vishwas-patil <vishpatil145@gmail.com>
2024-04-13 13:30:06 +0530

    Check newline context

--- add_numbers.rb
+++ add_numbers.rb
@@ -2,7 +2,13 @@
 def add(numbers)
   return 0 if numbers.empty?
 
-  numbers.split(',').map(&:to_i).sum
+  begin
+    raise ArgumentError, "Invalid input: #{numbers}" if numbers.end_with?(",\n")
+
+    numbers.gsub(/\n/, ',').split(',').map(&:to_i).sum
+  rescue ArgumentError => e
+    puts "Error: #{e.message}"
+  end
 end
 
 puts "Test Cases:"
@@ -10,3 +16,8 @@ puts "add(\"\"): #{add("")}"    # Output: 0
 puts "add(\"1\"): #{add("1")}"   # Output: 1
 puts "add(\"1,5\"): #{add("1,5")}" # Output: 6
 puts "add(\"1,5,6\"): #{add("1,5,6")}" # Output: 12
+puts "add new line(\"1\n2,3\"): #{add("1\n2,3")}" # Output: 6
+puts "---------Check this context--------------"
+puts "The following input is invalid: '1,\n'"
+puts "-----------------------------------------"
+puts "add new line(\"1,\n\"): #{add("1,\n")}" # Output: Invalid input
